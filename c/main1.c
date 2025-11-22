108168e5e23a1571f1dc126636c1a5bbd229fb2e
Kornel Łaba <kornellaba44@gmail.com>
2025-11-22 17:29:43 +0100

    Update VSCode config and add pointer example in main1.c
    Changed compiler paths in VSCode configuration files to use mingw64 instead of ucrt64, updated launch and task labels to gcc.exe, and enabled external console for debugging. Added a pointer example to main1.c to demonstrate usage of const int* and related printf statements.

--- c/main1.c
+++ c/main1.c
@@ -10,5 +10,13 @@ int main() {
     char ch = 'Z';
     float pi = 3.14;
 
+
+    const int* ptr = &num;
+
+    printf("%i\n", num);
+    num = 4;
+    printf("%i\n", ptr);
+    printf("%i\n", num);
+
     return 0;
 }
