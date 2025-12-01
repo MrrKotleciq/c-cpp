0f45e471d25ac3fd30b127e61e874e5f48aff9b8
Kornel Łaba <kornellaba44@gmail.com>
2025-12-01 17:50:53 +0100

    Add program to print name multiple times
    Added main2.c and compiled binary main2.exe. The program prompts the user for a name and a count, then prints the name the specified number of times.

---
+++ c/main2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of times you want to print your name: ");
+    scanf("%d", &n);
+    char name[30];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    while(n){
+        printf("%d. %s\n", n, name);
+        n--;
+    }
+}
